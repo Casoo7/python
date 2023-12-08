2cf3f74ad6edfb99bb932bbd2382c5ec493bc5bb
yr <827969491@qq.com>
2023-12-08 17:45:38 +0800

    画框

--- cnn/drawPanel.py
+++ cnn/drawPanel.py
@@ -2,16 +2,33 @@ import cv2
 import numpy as np
 
 def mouse_callback(event,x,y,flags,userdata):
+    print(startPt)
     if(userdata[1] == "l"):
+        # startPt = userdata[2]
+        # endPt = userdata[3]
         if(event==cv2.EVENT_LBUTTONDOWN):
-            cv2.line(userdata[0],(0,0),(500,500),(0,0,255),100,16)
-            cv2.imshow("aa",userdata[0]) 
+            if(startPt == (-1,-1)):
+                startPt = (x,y)
+            if(startPt != (-1,-1)):
+                endPt = (x,y)
+                cv2.line(img,startPt,endPt,(0,0,255),2,16)
+                cv2.imshow("aa",userdata[0])
+                startPt = (-1,-1)   
+                endPt = (-1,-1)
+        if(event==cv2.EVENT_MOUSEMOVE):
+            if(startPt != (-1,-1)):
+                endPt = (x,y)
+                cv2.line(userdata[0],startPt,endPt,(0,0,255),100,16)
+                cv2.imshow("aa",userdata[0])
     print(event,x,y,flags,userdata[1])
 
 
-img = np.zeros((1080,1920,3),np.uint8)
+img = np.zeros((540,860,3),np.uint8)
 cv2.imshow("aa",img) 
 flag = '0'
+
+startPt = (-1,-1)
+endPt = (-1,-1)
 userdata=[img,flag]
 cv2.setMouseCallback('aa',mouse_callback,userdata)
 
